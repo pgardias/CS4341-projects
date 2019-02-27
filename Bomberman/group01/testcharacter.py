70e9d0075bcc494e7bfa9f713dcf5fff7905649a
Danny Sullivan <djsully9@live.com>
2019-02-27 01:04:41 -0500

    astar, but still broken
    new implementation, but not returning best possible path, just returns all nodes and costs

--- Bomberman/group01/testcharacter.py
+++ Bomberman/group01/testcharacter.py
@@ -8,6 +8,7 @@ from events import Event
 
 gamma = 0.9
 
+
 class TestCharacter(CharacterEntity):
 	weights = []
 	learning_rate = 0.2
@@ -19,11 +20,11 @@ class TestCharacter(CharacterEntity):
 		self.move(next_action[0], next_action[1])
 		self.writeWeights()
 
-	def nearWall(self,wrld):
-		myPos = wrld.me(self).x,wrld.me(self).y
+	def nearWall(self, wrld):
+		myPos = wrld.me(self).x, wrld.me(self).y
 		exitPos = wrld.exitcell
-		dx = exitPos[0]-myPos[0]
-		dy = exitPos[1]-myPos[1]
+		dx = exitPos[0] - myPos[0]
+		dy = exitPos[1] - myPos[1]
 		if (dx < 0):
 			dx = -1
 		if (dx > 0):
@@ -32,8 +33,8 @@ class TestCharacter(CharacterEntity):
 			dy = -1
 		if (dy > 0):
 			dy = 1
-		if(0 <= myPos[0]+dx < wrld.width() and 0 <= myPos[1]+dy < wrld.height()):
-			if(wrld.wall_at(myPos[0]+dx, myPos[1]+dy)):
+		if (0 <= myPos[0] + dx < wrld.width() and 0 <= myPos[1] + dy < wrld.height()):
+			if (wrld.wall_at(myPos[0] + dx, myPos[1] + dy)):
 				return True
 		return False
 
@@ -63,13 +64,14 @@ class TestCharacter(CharacterEntity):
 			print('Action:', action, 'has qval:', q_vals[action])
 			print('distanceToExit:', self.distanceToExit(next_position, wrld))
 			print('explosionNextTurn:', self.explosionNextTurn(action, wrld))
+			print('Distance to monster:', self.distanceToMonster(action, wrld))
 
 		for key in q_vals.keys():
 			if q_vals[key] > best_qval:
 				best_action = key
 				best_qval = q_vals[key]
 
-		if(self.nearWall(wrld)):
+		if (self.nearWall(wrld)):
 			self.place_bomb()
 
 		return (best_action[0], best_action[1])
@@ -78,24 +80,24 @@ class TestCharacter(CharacterEntity):
 		return abs(x[1] - y[1]) + abs(x[0] - y[0])
 
 	def distanceToExit(self, pos, wrld):
-		origin = 0,0
+		origin = 0, 0
 
-		#return 1 - (self.astar(wrld,pos,wrld.exitcell)/self.astar(wrld,origin,wrld.exitcell))
+		# return 1 - (self.astar(wrld,pos,wrld.exitcell)/self.astar(wrld,origin,wrld.exitcell))
 		return 1 - (self.distance(pos, wrld.exitcell) / self.distance((0, 0), wrld.exitcell))
 
-	def distanceToMonster(self, wrld):
+	def distanceToMonster(self, action, wrld):
 		# get location of closest monster
 		monster_locations = []
 		for i in range(wrld.width()):
 			for j in range(wrld.height()):
-				if wrld.monster_at((i, j)):
+				if wrld.monsters_at(i, j):
 					monster_locations.append((i, j))
 
 		# get A* path length for each monster
 		monster_found = False
 		shortest_path_to_monster = 1e99999
 		for location in monster_locations:
-			path = self.astar(wrld, (self.x, self.y), location)
+			path = self.astar(wrld, (self.x + action[0], self.y + action[1]), location)
 			if path is not None:
 				if len(path) < shortest_path_to_monster:
 					shortest_path_to_monster = len(path)
@@ -119,7 +121,7 @@ class TestCharacter(CharacterEntity):
 		# else:
 		# 	return True
 		character = wrld.me(self)
-		startpos = self.x,self.y
+		startpos = self.x, self.y
 		character.x = self.x + move[0]
 		character.y = self.y + move[1]
 		sooner = wrld.next()
@@ -141,59 +143,91 @@ class TestCharacter(CharacterEntity):
 
 	def getValidMoves(self, wrld):
 		valid_moves = []
-		for i in [-1,0,1]:
-			for j in [-1,0,1]:
+		for i in [-1, 0, 1]:
+			for j in [-1, 0, 1]:
 				x_pos = self.x + i
 				y_pos = self.y + j
 				if wrld.exit_at(x_pos, y_pos):
 					return [(i, j)]
 				elif 0 <= x_pos < wrld.width() and 0 <= y_pos < wrld.height():
-					if (wrld.empty_at(x_pos, y_pos) or wrld.characters_at(x_pos,y_pos)) and not self.explosionNextTurn((x_pos, y_pos), wrld):
+					if (wrld.empty_at(x_pos, y_pos) or wrld.characters_at(x_pos, y_pos)) and not self.explosionNextTurn(
+							(x_pos, y_pos), wrld):
 						valid_moves.append((i, j))
 		return valid_moves
 
 	def getPriority(self, elem):
 		return elem[1]
 
+	# def astar(self, wrld, start, end):
+	# 	cameFrom = {}
+	# 	costSoFar = {}
+	# 	cameFrom[start] = None
+	# 	costSoFar[start] = 0
+	#
+	# 	front = []
+	# 	front.append((start, 0))
+	#
+	# 	while len(front) != 0:
+	# 		front.sort(key=self.getPriority)
+	# 		current = front.pop(0)
+	# 		self.set_cell_color(current[0][0], current[0][1], Fore.RED + Back.GREEN)
+	#
+	# 		if current[0] == end: # condition is never met
+	# 			path = []
+	# 			current = current[0]
+	# 			while current in cameFrom:
+	# 				current = cameFrom[current]
+	# 				path.append(current)
+	# 			path.pop()
+	# 			path.reverse()
+	# 			return path
+	#
+	# 		for neighbor in self.getValidSpotsFrom(current[0], wrld):
+	# 			cost = costSoFar[current[0]] + 1
+	# 			if neighbor not in costSoFar or cost < costSoFar[neighbor]:
+	# 				costSoFar[neighbor] = cost
+	# 				pVal = cost + self.distance(neighbor, end)
+	# 				front.append((neighbor, pVal))
+	# 				cameFrom[neighbor] = current[0]
+
+# new astar is not returning an actual path, instead finding costs to all cells. needs to actually construct a path using
+# this information, see the following for poor implementation:
+# https://www.redblobgames.com/pathfinding/a-star/implementation.html#python-astar
+
 	def astar(self, wrld, start, end):
+		frontier = []
+		frontier.append((start, 0))
+
 		cameFrom = {}
 		costSoFar = {}
 		cameFrom[start] = None
 		costSoFar[start] = 0
 
-		front = []
-		front.append((start, 0))
-
-		while len(front) != 0:
-			front.sort(key=self.getPriority)
-			current = front.pop(0)
-			self.set_cell_color(current[0][0], current[0][1], Fore.RED + Back.GREEN)
+		while len(frontier) != 0:
+			print(frontier)
+			frontier.sort(key=self.getPriority)
+			current = frontier[0]
+			frontier = frontier[1:len(frontier)]
 
 			if current[0] == end:
-				path = []
-				current = current[0]
-				while current in cameFrom:
-					current = cameFrom[current]
-					path.append(current)
-				path.pop()
-				path.reverse()
-				return path
+				break
 
-			for neighbor in self.getValidSpotsFrom(current[0], wrld):
-				cost = costSoFar[current[0]] + 1
-				if neighbor not in costSoFar or cost < costSoFar[neighbor]:
-					costSoFar[neighbor] = cost
-					pVal = cost + self.distance(neighbor, end)
-					front.append((neighbor, pVal))
-					cameFrom[neighbor] = current[0]
+			for next in self.getValidSpotsFrom(current[0], wrld):
+				newCost = costSoFar[current[0]] + 1
+				if next not in costSoFar or newCost < costSoFar[next]:
+					costSoFar[next] = newCost
+					priority = newCost + 1
+					frontier.append((next, priority))
+					cameFrom[next] = current
+		return cameFrom
 
 	def getValidSpotsFrom(self, start, wrld):
 		valid_moves = []
-		for i in [-1,0,1]:
-			for j in [-1,0,1]:
+		for i in [-1, 0, 1]:
+			for j in [-1, 0, 1]:
 				pos = (start[0] + i, start[1] + j)
-				if(0 <= pos[0] < wrld.width() and 0 <= pos[1] < wrld.height()):
-					if wrld.empty_at(pos[0],pos[1]) or wrld.monsters_at(pos[0],pos[1]) or wrld.exit_at(pos[0],pos[1]) or wrld.characters_at(pos[0],pos[1]):
-						valid_moves.append((i, j))
+				if 0 <= pos[0] < wrld.width() and 0 <= pos[1] < wrld.height():
+					if wrld.empty_at(pos[0], pos[1]) or wrld.monsters_at(pos[0], pos[1]) or wrld.exit_at(pos[0], pos[
+						1]) or wrld.characters_at(pos[0], pos[1]):
+						valid_moves.append(pos)
 		return valid_moves
-
